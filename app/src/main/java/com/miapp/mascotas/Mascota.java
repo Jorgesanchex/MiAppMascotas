0d640bf8df6d6f7a2e51290a550422fbf927aa56
Jorgesanchex <jtrujillo59@uabc.edu.mx>
2026-01-18 13:28:07 -0800

    Update Mascota.java

--- app/src/main/java/com/miapp/mascotas/Mascota.java
+++ app/src/main/java/com/miapp/mascotas/Mascota.java
@@ -1,16 +1,19 @@
 package com.miapp.mascotas;
 
 public class Mascota {
+    private int id;            // Identificador único
     private String nombre;
     private int imagenResId;
     private int rating;
 
-    public Mascota(String nombre, int imagenResId) {
+    public Mascota(int id, String nombre, int imagenResId) {
+        this.id = id;
         this.nombre = nombre;
         this.imagenResId = imagenResId;
         this.rating = 0;
     }
 
+    public int getId() { return id; }
     public String getNombre() { return nombre; }
     public int getImagenResId() { return imagenResId; }
     public int getRating() { return rating; }
